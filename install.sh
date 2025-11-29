7ab4b14aac460d31418ca9c8bf7dd0e39e4fb9da
eddydavies <eddy@fortick.io>
2025-11-29 16:08:32 +0000

    Add mac_config.yml for comprehensive macOS setup and update install.sh to execute the new playbook. Remove unnecessary steps from mac_setup.yml.

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ xcode-select --install 2>/dev/null || true
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)" 2>/dev/null || eval "$(/usr/local/bin/brew shellenv)"
 brew install ansible
+ansible-playbook mac_config.yml 
 ansible-playbook mac_setup.yml 
